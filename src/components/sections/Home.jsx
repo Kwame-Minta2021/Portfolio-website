a4d657c12698168a039fcb05349827f1c639f337
Frederick Kwame Minta <frederickminta@gmail.com>
2023-10-19 06:59:01 +0000

    My Portfolio Website

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -32,24 +32,28 @@ const Home = () => {
         <section id='home'
         >
             <motion.div initial={{opacity: 0, x: "-50vw"}}
-            animate={{opacity: 1, x: 0, transition: {delay: 1.4, duration: 0.5}}} className='home-left'>
+            animate={{opacity: 1, x: 0, transition: {delay: 1.4, duration: 0.8, ease: "easeOut"}}}
+			className='home-left'>
                 <h1>
                     Hi! I'm <span>Fredrick Minta Newton</span>
                 </h1>
                 <h2>
-                    Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Repellat beatae, vitae,
-                    dicta, voluptatibus omnis asperiores iste
-                    ea fuga tempore veritatis provident. Sint
-                    esse quaerat culpa minima quod debitis?
-                    Magnam, tempore?
+                    This portfolio showcases a range of projects
+					I have worked on. It also contains my
+					experiences, demonstrating my
+					proficiency in designing circuits and coding 
+					firmware to turn concepts into functional hardware 
+					solutions. This portfolio serves as a testament to 
+					my abilities and innovative approach to computer
+					engineering.
                 </h2>
             </motion.div>
-            <div className='home-right'>
+            <motion.div className='home-right' initial={{opacity: 0, x: "50vw"}}
+            animate={{opacity: 1, x: 0, transition: {delay: 1.4, duration: 0.8, ease: "easeOut"}}}>
                 <Lottie options={defaultOptions}
                 height={!isMobile?400:200} width={!isMobile?400:270}
                 />
-            </div>
+            </motion.div>
         </section>
     )
 }
